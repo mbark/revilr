7113216432be2509f1258e3a971633b03aa87722
Martin, Barksten <martin.barksten@gmail.com>
2013-05-22 14:43:46 +0200

    Updated server to handle each separate revil

--- server/server.go
+++ server/server.go
@@ -5,12 +5,30 @@ import (
     "net/http"
 )
 
-func revilHandler(w http.ResponseWriter, r *http.Request) {
+func linkHandler(w http.ResponseWriter, r *http.Request) {
     values := r.URL.Query()
-    fmt.Println("Reviled",values["page"])
+    fmt.Println("Reviled link",values["val"], "with comment", values["c"])
+}
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+    values := r.URL.Query()
+    fmt.Println("Reviled page",values["val"], "with comment", values["c"])
+}
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+    fmt.Println("Reviled image",values["val"], "with comment", values["c"])
+}
+
+func selectionHandler(w http.ResponseWriter, r *http.Request) {
+    values := r.URL.Query()
+    fmt.Println("Reviled selection",values["val"], "with comment", values["c"])
 }
 
 func main() {
-    http.HandleFunc("/revil", revilHandler)
+    http.HandleFunc("/revil/link", linkHandler)
+    http.HandleFunc("/revil/page", pageHandler)
+    http.HandleFunc("/revil/image", imageHandler)
+    http.HandleFunc("/revil/selection", selectionHandler)
     http.ListenAndServe(":8080", nil)
 }
