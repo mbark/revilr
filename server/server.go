5a8c9276aec0aaf462b5361e264a1f80a48a89e1
David Rydberg <rydan4@gmail.com>
2013-05-22 16:34:22 +0200

    Hipper names

--- server/server.go
+++ server/server.go
@@ -7,28 +7,28 @@ import (
 
 func linkHandler(w http.ResponseWriter, r *http.Request) {
     values := r.URL.Query()
-    fmt.Println("Reviled link",values["val"], "with comment", values["c"])
+    fmt.Println("Revild link",values["val"], "with comment", values["c"])
 }
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
     values := r.URL.Query()
-    fmt.Println("Reviled page",values["val"], "with comment", values["c"])
+    fmt.Println("Revild page",values["val"], "with comment", values["c"])
 }
 
 func imageHandler(w http.ResponseWriter, r *http.Request) {
 	values := r.URL.Query()
-    fmt.Println("Reviled image",values["val"], "with comment", values["c"])
+    fmt.Println("Revild image",values["val"], "with comment", values["c"])
 }
 
 func selectionHandler(w http.ResponseWriter, r *http.Request) {
     values := r.URL.Query()
-    fmt.Println("Reviled selection",values["val"], "with comment", values["c"])
+    fmt.Println("Revild selection",values["val"], "with comment", values["c"])
 }
 
 func main() {
-    http.HandleFunc("/revil/link", linkHandler)
-    http.HandleFunc("/revil/page", pageHandler)
-    http.HandleFunc("/revil/image", imageHandler)
-    http.HandleFunc("/revil/selection", selectionHandler)
+    http.HandleFunc("/revilr/link", linkHandler)
+    http.HandleFunc("/revilr/page", pageHandler)
+    http.HandleFunc("/revilr/image", imageHandler)
+    http.HandleFunc("/revilr/selection", selectionHandler)
     http.ListenAndServe(":8080", nil)
 }
