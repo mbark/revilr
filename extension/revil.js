1075d3ef1f8048bffd0e73ffb2d931614ef1174c
Martin, Barksten <martin.barksten@gmail.com>
2013-05-22 15:53:45 +0200

    Solved Content Security Policy error
    https://developer.chrome.com/extensions/contentSecurityPolicy.html

--- extension/revil.js
+++ extension/revil.js
@@ -1,6 +1,3 @@
-/**
- * Renders the URL for the image, trimming if the length is too long.
- */
 function renderText(url) {
   document.getElementById('urlField').value = url;
 }
@@ -9,12 +6,9 @@ function revilNow() {
   alert("Sent:\n\n" + document.getElementById('urlField').value + "\n" + document.getElementById('commentField').value);
 }
 
-/**
- * Load the image in question and display it, along with its metadata.
- */
-document.addEventListener("DOMContentLoaded", function () {
-  // The URL of the image to load is passed on the URL fragment.
-  var url = window.location.hash.substring(1);
 
+document.addEventListener("DOMContentLoaded", function () {
+  var url = window.location.hash.substring(1);
+  document.querySelector('button').addEventListener('click', revilNow);
   renderText(url);
 });
