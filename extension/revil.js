5e1acfae1bf68d773d05466ea7396637a41e6046
Martin, Barksten <martin.barksten@gmail.com>
2013-05-23 18:32:23 +0200

    Posting using jQuery

--- extension/revil.js
+++ extension/revil.js
@@ -5,28 +5,21 @@ function renderText(url, comment) {
 
 function revilNow() {
 	var type = getAndDeleteFromStorage('type');
-	var url = document.getElementById('urlField').value;
-	var comment = document.getElementById('commentField').value;
-
 	var targetUrl = "http://127.0.0.1:8080/revilr/" + type;
-	var params = "url=" + url + "&c=" + comment;
 
-	postRevilToServer(targetUrl, params);
-}
-
-function postRevilToServer(targetUrl, params) {
-	var http = new XMLHttpRequest();
-	http.open("POST", targetUrl, true);
-	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	http.setRequestHeader("Content-length", params.length);
-	http.setRequestHeader("Connection", "close");
-	http.onreadystatechange = function() {//Call a function when the state changes.
-		if(http.readyState == 4 && http.status == 200) {
-			alert("Sent succesfully!");
-			window.close();
-		}
-	}
-	http.send(params);
+	var posting = $.post(targetUrl, {
+		url: document.getElementById('urlField').value,
+		c: document.getElementById('commentField').value
+	});
+	posting.done(function() {
+		alert("Reviled!");
+	});
+	posting.fail(function() {
+		alert("Failed to revil!");
+	});
+	posting.always(function() {
+		window.close();
+	});
 }
 
 function getAndDeleteFromStorage(item) {
