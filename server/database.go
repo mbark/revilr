ba18115cf2131d644b50d682e46908736f8d2afd
David Rydberg <rydan4@gmail.com>
2013-05-27 20:46:13 +0200

    Separated database stuff from server

---
+++ server/database.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+)
+
+var database *sql.DB
+
+func getDatabase() (db *sql.DB, err error) {
+	dbPath := "./revils.db"
+
+	//check if file exists
+	if exists, _ := fileExists(dbPath); exists {
+		db, err = sql.Open("sqlite3", "./"+dbPath)
+	} else {
+		db, err = instantiateDatabase(dbPath)
+	}
+	return
+}
+
+func fileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func instantiateDatabase(dbPath string) (db *sql.DB, err error) {
+	db, err = sql.Open("sqlite3", dbPath)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	sqls := []string{
+		"create table revil (url text not null primary key, type text, comment text)",
+	}
+
+	for _, sql := range sqls {
+		_, err = db.Exec(sql)
+		if err != nil {
+			fmt.Printf("%s: %s\n", err, sql)
+		}
+	}
+
+	return
+}
+
+func insertIntoDatabase(rev revil) error {
+	stmt, err := database.Prepare("insert into revil(url, type, comment) values(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	stmt.Exec()
+	_, err = stmt.Exec(rev.Url, rev.Type, rev.Comment)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getAllValuesInDatabase() {
+	rows, err := database.Query("select url, type, comment from revil")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var url string
+		var rtype string
+		var comment string
+		rows.Scan(&url, &rtype, &comment)
+		revil{Type: rtype, Url: url, Comment: comment}.printRevil();
+	}
+}
--- server/server.go
+++ server/server.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/mattn/go-sqlite3"
 	"net/http"
-	"os"
 )
-
-var database *sql.DB
 
 func getRevil(request *http.Request, t string) revil {
 	return revil{Type: t, Url: request.FormValue("url"), Comment: request.FormValue("c")}
@@ -46,79 +40,6 @@ func selectionHandler(w http.ResponseWriter, request *http.Request) {
 	}
 }
 
-func getDatabase() (db *sql.DB, err error) {
-	dbPath := "./revils.db"
-
-	//check if file exists
-	if exists, _ := fileExists(dbPath); exists {
-		db, err = sql.Open("sqlite3", "./"+dbPath)
-	} else {
-		db, err = instantiateDatabase(dbPath)
-	}
-	return
-}
-
-func fileExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
-}
-
-func instantiateDatabase(dbPath string) (db *sql.DB, err error) {
-	db, err = sql.Open("sqlite3", dbPath)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	sqls := []string{
-		"create table revil (url text not null primary key, type text, comment text)",
-	}
-
-	for _, sql := range sqls {
-		_, err = db.Exec(sql)
-		if err != nil {
-			fmt.Printf("%s: %s\n", err, sql)
-		}
-	}
-
-	return
-}
-
-func insertIntoDatabase(rev revil) error {
-	stmt, err := database.Prepare("insert into revil(url, type, comment) values(?, ?, ?)")
-	if err != nil {
-		return err
-	}
-	stmt.Exec()
-	_, err = stmt.Exec(rev.Url, rev.Type, rev.Comment)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func getAllValuesInDatabase() {
-	rows, err := database.Query("select url, type, comment from revil")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var url string
-		var rtype string
-		var comment string
-		rows.Scan(&url, &rtype, &comment)
-		fmt.Println(url, rtype, comment)
-	}
-}
-
 func main() {
 	db, err := getDatabase()
 	if err != nil {
@@ -129,7 +50,7 @@ func main() {
 	defer database.Close()
 
 	//uncomment to verify it works
-	//getAllValuesInDatabase()
+	getAllValuesInDatabase()
 
 	http.HandleFunc("/revilr/link", linkHandler)
 	http.HandleFunc("/revilr/page", pageHandler)
