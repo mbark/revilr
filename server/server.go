ff181d75fbe9afd66b5130bdc3cfe8fcf73af918
Martin, Barksten <martin.barksten@gmail.com>
2013-06-16 14:19:29 +0200

    General improvements to names, methods etc

--- server/server.go
+++ server/server.go
@@ -9,56 +9,10 @@ import (
 
 const lenPath = len("/revilr/")
 
-var postTypeValidator = regexp.MustCompile("^(page|image|selection)$")
-var getTypeValidator = regexp.MustCompile("^(page|image|selection|)$")
+var validTypes = regexp.MustCompile("^(page|image|selection)$")
 
 func getRevil(request *http.Request, t string) revil {
 	return revil{Type: t, Url: request.FormValue("url"), Comment: request.FormValue("c")}
-}
-
-func httpHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		postHandler(w, r)
-	} else if r.Method == "GET" {
-		getHandler(w, r)
-	}
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	rType := r.URL.Path[lenPath:]
-	if !postTypeValidator.MatchString(rType) {
-		http.NotFound(w, r)
-		return
-	}
-	rev := getRevil(r, rType)
-	rev.printRevil()
-	insertIntoDatabase(rev)
-}
-
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	rType := r.URL.Path[lenPath:]
-	if !getTypeValidator.MatchString(rType) {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Println(rType)
-	rev := getRevilOfType(rType)
-
-	htmlFile := "templates/" + rType + ".html"
-	t, err := template.ParseFiles(htmlFile)
-
-	if err != nil {
-		fmt.Println(err)
-		http.NotFound(w, r)
-	}
-	t.Execute(w, rev)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	rev := getAllRevilsInDatabase()
-
-	t, _ := template.ParseFiles("templates/index.html")
-	t.Execute(w, rev)
 }
 
 func main() {
@@ -70,11 +24,51 @@ func main() {
 	database = db
 	defer database.Close()
 
-	//uncomment to verify it works
-	printAllRevilsInDatabase()
-
 	http.HandleFunc("/revilr/", httpHandler)
 	http.HandleFunc("/revilr", indexHandler)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("templates/resources"))))
 	http.ListenAndServe(":8080", nil)
+}
+
+func httpHandler(writer http.ResponseWriter, request *http.Request) {
+	revilType := parseType(request)
+	if !validTypes.MatchString(revilType) {
+		http.NotFound(writer, request)
+		return
+	}
+
+	if request.Method == "POST" {
+		postHandler(request, revilType)
+	} else if request.Method == "GET" {
+		getHandler(writer, request, revilType)
+	}
+}
+
+func parseType(request *http.Request) string {
+	return request.URL.Path[lenPath:]
+}
+
+func indexHandler(writer http.ResponseWriter, request *http.Request) {
+	rev := getAllRevilsInDatabase()
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(writer, rev)
+}
+
+func postHandler(request *http.Request, revilType string) {
+	rev := getRevil(request, revilType)
+	rev.printRevil()
+	insertIntoDatabase(rev)
+}
+
+func getHandler(writer http.ResponseWriter, request *http.Request, revilType string) {
+	rev := getRevilOfType(revilType)
+
+	htmlFile := "templates/" + revilType + ".html"
+	t, err := template.ParseFiles(htmlFile)
+
+	if err != nil {
+		fmt.Println(err)
+		http.NotFound(writer, request)
+	}
+	t.Execute(writer, rev)
 }
