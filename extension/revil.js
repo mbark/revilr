59756bc0dd1a06bb247f2aa1e7baa814dc6d7d30
David Rydberg <rydan4@gmail.com>
2013-06-26 19:52:31 +0200

    More styling...

--- extension/revil.js
+++ extension/revil.js
@@ -38,10 +38,31 @@ function clearStorage() {
   	window.localStorage.removeItem('comment');
 }
 
+function setTypeDiv(text, color) {
+	var element = document.getElementById('revil-type');
+	element.innerHTML = text;
+	element.style.backgroundColor = color;
+}
+
 $(document).ready(function() {
 	document.getElementById('buttonOk').innerHTML = 'Revil ' + getFromStorage('type') + '!';
   	document.getElementById('buttonOk').addEventListener('click', revilNow);
   	document.getElementById('buttonCancel').addEventListener('click', exit);
+		
+	var type = getFromStorage('type');
+	if (type == 'selection') {
+		document.getElementById('urlField').setAttribute('readonly');
+		document.getElementById('commentField').setAttribute('readonly');
+		setTypeDiv(type, '#BF4330');
+	} else if (type == 'page') {
+		document.getElementById('urlField').setAttribute('readonly');
+		setTypeDiv(type, '#3AA6D0');
+	} else if (type == 'image') {
+		document.getElementById('urlField').setAttribute('readonly');
+		setTypeDiv(type, '#BF9330');
+	} else {
+		exit();
+	}
 
   	var url = getFromStorage('url');
   	var comment = getFromStorage('comment');
